1a637f52bc09855f53c703e31f1a4ae269b38afe
Rahul Chauhan <chauhanr371@gmail.com>
2021-01-31 20:17:56 +0530

    updated next-js-2021

---
+++ _next/static/ouMpaJ61pp8326uXb_MQQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-b73db69b77d6f853772d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-df10bbd8af737075a53d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
