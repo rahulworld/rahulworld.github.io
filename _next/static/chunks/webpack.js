d024aedee9c9a6c19f05993454c6c6fb656ddce2
Rahul Chauhan <chauhanr371@gmail.com>
2021-01-31 20:22:04 +0530

    updated 2021-next-js

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d7a1eecad2fbadadb066";
+/******/ 	var hotCurrentHash = "c2debc225133143c1801";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
